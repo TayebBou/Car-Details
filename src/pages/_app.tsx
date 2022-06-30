38c4fb6556ff108f20f788814b4e32cee8f1f05e
TayebBou <tayebbousfiha72@gmail.com>
2022-06-30 17:10:43 +0200

    Add layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Layout from '../components/hoc/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Car Details</title>
-        <meta name="description" content="Car Details is a website where user can look at cars and their informations and contact sellers." />
+        <meta
+          name="description"
+          content="Car Details is a website where user can look at cars and their informations and contact sellers."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  const router = useRouter()
+  
   return (
     <div className={styles.container}>
     </div>
